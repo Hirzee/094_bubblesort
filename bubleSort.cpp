b6f5e2f797ea316510257b2c0b43e65259bacc40
Hirzee <faruqhirzi698@gmail.com>
2026-03-05 12:06:50 +0700

    void Display

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -7,23 +7,46 @@ int n;
 
 void input() {
     while (true) {
-        cout << "Masukan banyaknya elemen pada array : ";
+        cout << "Masukkan banyaknya elemen pada array: ";
         cin >> n;
-
         if (n <= 20)
             break;
-        else
-            cout << "\nArray dapat mempunyai maksimal 20 elemen\n";
-    }
-
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    } 
     cout << endl;
-    cout << "====================" << endl;
+    cout << "======================" << endl;
     cout << "Masukkan Elemen Array" << endl;
-    cout << "====================" << endl;
+    cout << "======================" << endl;
 
     for (int i = 0; i < n; i++) {
         cout << "Data ke-" << (i + 1) << ": ";
         cin >> arr[i];
-    }
+    }   
+}
+
+void bubbleSort() {
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+
+    }  while (pass <= n - 1);
+
 }
 
